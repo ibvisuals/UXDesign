2ef053cb8265a90c8b72c18022caeb8f20090617
Intyaud Banton <65741007+ibvisuals@users.noreply.github.com>
2021-06-13 00:33:53 -0500

    Initial commit

---
+++ app/src/main/java/com/ibvisuals/uxdesign/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.ibvisuals.uxdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        TextView alreadyRegistered = findViewById(R.id.alreadyRegisteredText);
+        alreadyRegistered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+    }
+
+}
